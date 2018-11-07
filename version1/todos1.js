d2b7ddf94678d6741ed8441b768347de949203b0
Jackie <JGtheophila19@gmail.com>
2018-11-07 10:59:28 -0500

    console logging todos

---
+++ version1/todos1.js
@@ -0,0 +1,11 @@
+// it should have a place to store
+var todos = ['item 1', 'item 2', 'item 3']; // array
+
+// It should have a way to display todos
+console.log(todos); 
+// prints out: 
+// [ 'item 1', 'item 2', 'item 3' ]
+
+console.log("My todos:", todos);
+// prints out:
+// My todos: [ 'item 1', 'item 2', 'item 3' ]
