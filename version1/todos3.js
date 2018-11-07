84cf24e177ee520d9d1f9ef4541495a3f75c27c6
Jackie <JGtheophila19@gmail.com>
2018-11-07 11:01:32 -0500

    change a todo

---
+++ version1/todos3.js
@@ -0,0 +1,8 @@
+var todos = ['item 1', 'item 2', 'item 3']; 
+
+
+// It should have a way to change a todo
+
+todos[0] = 'item1 update'; // becomes the new value
+
+// [ 'item1 update', 'item 2', 'item 3', 'item 4' ]
