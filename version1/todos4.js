68a4f5604f40e4c00adb95e4377c3824954a1e60
Jackie <JGtheophila19@gmail.com>
2018-11-07 11:02:49 -0500

    Deleting in the todo array

---
+++ version1/todos4.js
@@ -0,0 +1,13 @@
+var todos = ['item 1', 'item 2', 'item 3']; 
+
+// It should have a way to delete a todo
+
+todos.splice(0, 1); 
+
+// (0 = position where to start deleting things in the array, 
+
+// 1 = how many item to delete in the array)
+
+// what was removed = [ 'item1 update' ]
+
+// new array = [ 'item 2', 'item 3', 'item 4' ]
