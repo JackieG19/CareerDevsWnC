8ad4a9984ce90ff1d2bc7158015975cd74aaf591
Jackie <JGtheophila19@gmail.com>
2018-11-10 13:01:05 -0500

    A function to display todos

--- version2/function3.js
+++ version2/function3.js
@@ -1,42 +1,13 @@
-// Function add \\
- 
- function addTodos() {
-     todos.puch('new todos');
- }
- addTodos()
- 
- // console - print
- todos
- ['item 1', 'item 2', 'item 3', 'new todos']
- 
- displayTodos()
- My Todos: ['item 1', 'item 2', 'item 3', "new todos'"]
- 
- 
- //-----------------------------------
- 
- 
- function addTodos() {
-     todos.puch('new todos');
-     displayTodos();
- }
- 
- addTodos()
- 
- // console - print
- My Todos: ['item 1', 'item 2', 'item 3', 'new todos']
- 
- 
- //------------------------------------
- 
- 
- function addTodos(todos) {
-     todos.puch(todos);
-     displayTodos();
- }
- addTodos('hello there')
- 
- // console - print
- My Todos: ['item 1', 'item 2', 'item 3', 'hello there']
- 
+// It should have a function to display todos
+
+var todos = ['item 1', 'item 2', 'item 3'];
+
+function displayTodos() {
+    console.log("My todos:", todos);
+}
+
+displayTodos();
+
+// print out:
+// My todos: ["item 1", "item 2", "item 3"]
  
