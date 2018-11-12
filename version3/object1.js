c283f05cbfa20d93bd9b5b637b581854a67c61ec
Jackie <JGtheophila19@gmail.com>
2018-11-11 19:14:07 -0500

    This is an object

--- version3/object1.js
+++ version3/object1.js
@@ -1,29 +1,22 @@
-// OBJECTS \\
-
-{
-    operatingSystem mac,
-    screenSize 15 inches,
-    purchaseYear 2011
-}
-    
-    
+// this is an object
 var myComputer = {
     operatingSystem: 'mac',
     screenSize: '15 inches',
     purchaseYear: 2011
 };
 
-// console - print
+console.log(myComputer);
+// {operatingSystem: 'mac', screenSize: '15 inches', purchaseYear: 2011}
 
-myComputer
-object {operatingSystem: 'mac', screenSize: '15 inches', purchaseYear: 2011}
 
-myComputer.operatingSystem
-"mac"
+console.log(myComputer.screenSize);
+// 15 inches
 
-myComputer.screenSize
-"15 inches"
 
-myComputer.purchaseYear
-2011
+console.log(myComputer.operatingSystem);
+// mac
+
+
+console.log(myComputer.purchaseYear);
+// 2011
 
