c4396223a80560134f2c4529ac4f5a63340c32ef
Jackie <JGtheophila19@gmail.com>
2018-11-13 12:19:46 -0500

    Store todos array in an object

--- version3/object3.js
+++ version3/object3.js
@@ -1,35 +1,8 @@
-// Array on an Object \\
 
-var todos = ['item 1', 'item 2', 'item 3'];
+//  It should store the todos array on an object
 
-var todoList{
-    todos: ['item 1', 'item 2', 'item 3']
-};
-
-// console - print
-todoList
-    Object{todos: Array[3]}
-        todos: Array[3]
-            0:"item 1"
-            1:"item 2"
-            2:"item 3"
-            length: 3
-            __proto__: Array[0]
-            __proto__: Object
-
-
-// Display Todos Method \\
-
-var todoList{
-    todos: ['item 1', 'item 2', 'item 3'],
-    displayTodos function() {                   function displayTodos() {
-     console.log("My Todos", this.todos);       console.log("My Todos", todos);
-    }
-};
-
-// console - print
-todoList.displayTodos();
-My Todos: ['item 1', 'item 2', 'item 3']
-
+var todoList = {
+    todos: ["item 1", "item 2", "item 3"]
+}
 
 
