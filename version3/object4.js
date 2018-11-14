2d53db16ab744d569d112cb8265514494c8fd94e
Jackie <JGtheophila19@gmail.com>
2018-11-14 18:23:23 -0500

    Display todos method
    creating a function to display the todos list inside an object

--- version3/object4.js
+++ version3/object4.js
@@ -1,41 +1,17 @@
-// Add Todo Method \\
+// It should have a displayTodos method
 
-var todoList{
-    todos: ['item 1', 'item 2', 'item 3'],
-    displayTodos function() {
-     console.log("My Todos", this.todos);
-    }
-    addTodos: function(todo) {
-        this.todos.push(todo);
-        this.displayTodos();
+var todoList = {
+    todos: ["item 1", "item 2", "item 3"],
+    displayTodos: function(){ // this is an anonymos function
+    console.log("My Todos:", this.todos);
     }
 };
 
-// console - print
-todoList.addTodos('pluncker');
-My Todos: ['item 1', 'item 2', 'item 3', 'pluncker']
+// Function, when they are methods, they dont need to name.
 
+// anonymos function - because when we run the function we're 
+// going to use the property name instead of the function name
 
-
-// Change Todo Method \\ 
-
-
-var todoList{
-    todos: ['item 1', 'item 2', 'item 3'],
-    displayTodos function() {
-     console.log("My Todos", this.todos);
-    },
-    addTodos: function(todo) {
-        this.todos.push(todo);
-        this.displayTodos();
-    },
-    changeTodos: function(position, newValue) {
-        this.todos[position] = newValue;
-        this.displayTodos();
-    }
-};
-
-// console - print
-todoList.changeTodos(0, 'first');
-My Todos: ['first', 'item 2', 'item 3', 'pluncker']
-
+// this.todos - refer to the todos properly on the object we're on
+// When you're in a method you can accessthe object you're on 
+// using this keyword and use the . to get a specific property
