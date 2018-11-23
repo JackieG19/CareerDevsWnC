0535d3b99dcfffc3966c8f9f0aa34b8e5dc51d8b
Jackie <JGtheophila19@gmail.com>
2018-11-22 23:31:34 -0500

    addTodo using the push method

--- version3/object5.js
+++ version3/object5.js
@@ -1,4 +1,4 @@
-// It should have an addTodo method
+// It should have an addTodo method 
 
 var todoList = {
     todos: ["item 1", "item 2", "item 3"],
@@ -6,7 +6,12 @@ var todoList = {
     console.log("My Todos:", this.todos);
     },
     addTodos: function(todo){ // new method
-        this.todos.push(todo); // this will refer to the todos array
+        
+        // this will refer to the todos array
+        this.todos.push(todo); // .push() method: Add items to the end of an array
+        // console.log("item 4")
+        
         this.displayTodos(); // this call the displayTodos function
+        // My Todos: ["item 1", "item 2", "item 3", "item 4"]
     }
 };
