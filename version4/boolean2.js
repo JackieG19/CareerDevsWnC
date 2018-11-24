09740547fe45819a40c34cb854f10d3190a45f92
Jackie <JGtheophila19@gmail.com>
2018-11-24 18:01:25 -0500

    Changing a property within the array created

--- version4/boolean2.js
+++ version4/boolean2.js
@@ -1,53 +1,38 @@
-var todoList {
-    todos: ['item 1', 'item 2', 'item 3'],
-    displayTodos function() {
-     console.log("My Todos", this.todos);
-    },/*
-(a.)--addTodos: function(todo) {
-        this.todos.push(todo);      
-        this.displayTodos();
-    }, */
-(b.)--changeTodos: function(position, newValue) {
-        this.todos[position] = newValue;
-        this.displayTodos();
-    },/*
-    
-    deleteTodos: function(position) {
-        this.splice(position, 1);
-        this.displayTodos();
+// todoList.changeTodo should change the todoText property
+
+var todoList = {
+    todos: [],  // ex. ["item 1", "item 8", "item 3", "item 4"]
+    displayTodos: function() {
+    console.log("My Todos:", this.todos);
     },
-(c.) <----------------------->
     
+    addTodos: function(todoText) {  // ex. ("item 5")
+        this.todos.push({ 
+        todoText: todoText, //  todoText: "item 5"
+        completed: false 
+        });
+        
+        this.displayTodos();
+        // example:
+        // My Todos: [{ todoText: 'item 1', completed: false}, - 0
+        // {todoText: 'item 8', completed: false}, - 1
+        // {todoText: 'item 3', completed: false}, - 2
+        // {todoText: 'item 4', completed: false}, - 3
+        // {todoText: 'item 5', completed: false }] - 4
+    },
+    
+    changeTodos: function(position, todoText) { // ex. (1, "item 2")
+        // input a position in the array and a random string
+        
+        this.todos[position].todoText = todoText; // this.todos[1].todoText = "item 2"
+        // this is grabbing the array position from an object in the addTodos method
+        
+        this.displayTodos();  
+        // example:
+        // My Todos: [{ todoText: 'item 1', completed: false}, - 0
+        // {todoText: 'item 2', completed: false}, - 1
+        // {todoText: 'item 3', completed: false}, - 2
+        // {todoText: 'item 4', completed: false}, - 3
+        // {todoText: 'item 5', completed: false }] - 4
+    }
 };
-{
-    todo.Text: 'item 1',
-    completed: ___ //booleans true or fasle
-} */
-
-
-// change the todoText property \\
-
-
-b.)  changeTodos: function(position, todoText) {
-        this.todos[position].todoText = todoText;
-        this.displayTodos();
-    },
-
-// console - print    
-todoList.addTodo('first try')
-    My Todos [Object]
-       0: Object
-          completed: fasle
-          todoText: 'first try'
-          __proto__: Object
-       length: 1
-     __proto__: Array[0]
-     
-todoList.changeTodo(0, 'second try')  
- My Todos [Object]
-       0: Object
-          completed: fasle
-          todoText: 'second try'
-          __proto__: Object
-       length: 1
-     __proto__: Array[0]
