f7bc447aca01d98d69fc600ece271246b8925f56
Jackie <JGtheophila19@gmail.com>
2018-12-27 23:43:27 -0500

    displayTodos should tell you if .todos is empty

--- version5/loops3.js
+++ version5/loops3.js
@@ -1,18 +1,16 @@
 // displayTodos should tell you if .todos is empty
-
-
-<-------NOTES----------------------------------------->   
-            // if there is no todos /OR
-            // if this.todos.length is equal to 0 /OR
-            // if this.todos.length === 0          
-            // (=== - compares different value)
-     if (this.todos.length === 0){
-        // console.log('your todos list is empty!')
-     // else
-     } else {
-        // print as normal
-     }
-    },
+ 
+// if there is no todos /OR
+// if this.todos.length is equal to 0 /OR
+// if this.todos.length === 0          
+// (=== - compares different value)
+     
+if (this.todos.length === 0){
+   // console.log('your todos list is empty!')
+ } else {
+       // print as normal
+    }
+ },
     
 
 if (condition) {
@@ -21,32 +19,21 @@ if (condition) {
     // code that run if condition is false
 }
 
-<-------NOTES-----------------------------------------> 
-
-
 
 var todoList {
     todos: [ ],
     displayTodos function() {
      if (this.todos.length === 0){
         console.log('your todos list is empty!')
-     }//if 
+          // todoList.displayTodos();
+          // your todos list is empty!
+     }
      else {
         console.log("My Todos:");
         for(var i = 0; i < this.todos.length; i++){
            console.log(this.todos[i].todoText);
-        }//for
-    },//else
-  }//function
-
-
-// console - print  
-todoList.displayTodos();
-your todos list is empty!
-
-todoList.addTodo('an item');
-My Todos:
-an item
-
-todoList.deleteTodo(0);
-your todos list is empty!
+             // todoList.addTodo('an item');
+             // My Todos: an item
+        }
+    },
+  }
