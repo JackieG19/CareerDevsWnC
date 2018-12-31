aed57f00ed12880cefbfff5693ee964f0564bc77
Jackie <JGtheophila19@gmail.com>
2018-12-30 21:19:49 -0500

    If everything is true, make everything false

--- version6/toggle1.js
+++ version6/toggle1.js
@@ -8,43 +8,18 @@ var todoList {
         console.log('your todos list is empty!')
      } 
         else {
-        console.log("My Todos:");
-        for(var i = 0; i < this.todos.length; i++){
+            console.log("My Todos:");
+            for(var i = 0; i < this.todos.length; i++){
         
     if (this.todos[i].completed === true) {
-        console.log('(x)', this.todos[i].todoText);
-           
+        console.log('(x)', this.todos[i].todoText);   
         }   
-            else {
-                console.log('( )', this.todos[i].todoText);
+        else {
+            console.log('( )', this.todos[i].todoText);
            }
         }
     },
 }
-    
-addTodos: function(todoText) {
-        this.todos.push({
-        todoText: todoText,
-        completed: fasle
-        });
-        this.displayTodos();
-    },
-    
-changeTodos: function(position, todoText) {
-        this.todos[position].todoText = todoText;
-        this.displayTodos();
-    },
-
-deleteTodos: function(position) {
-        this.splice(position, 1);
-        this.displayTodos();
-    },
-
-toggleCompleted: function(position) {
-        var todo = this.todos[position];
-        todos.completed = !todos.completed;
-        this.displayTodos();
-},
 
 toggleAll: function(position) {
 
@@ -55,10 +30,9 @@ toggleAll: function(position) {
     for (var i = 0, i = totalTodos, i++){
       if (this.todos[i].completed === true){
         completedTodos++;
-        }//for
-    }//if
+        }
+    }
     
-
     // Case 1: If everything's true, make everything false
     if (completedTodos === totalTodos) {
     
@@ -66,30 +40,28 @@ toggleAll: function(position) {
     for (var i = 0, i = totalTodos, i++){
       this.todos[i].completed = false){
         completedTodos++;
-        }//this
-      }//for
-      
-      this.displayTodos();
-      
-    }//if
-};//function
+        }
+      }
+      this.displayTodos(); 
+    }
+};
 
 
 // console - prints
-todoList.addTodo('first');          todoList.addTodo('second');
-My Todos:                           My Todos:
-( ) first                           ( ) first
-                                    ( ) second
+// todoList.addTodo('first');          todoList.addTodo('second');
+// My Todos:                           My Todos:
+// ( ) first                           ( ) first
+//                                     ( ) second
 
 
-todoList.toggleCompleted(0);        todoList.toggleCompleted(1);
-My Todos:                           My Todos:
-(x) first                           (x) first
-( ) second                          (x) second
+// todoList.toggleCompleted(0);        todoList.toggleCompleted(1);
+// My Todos:                           My Todos:
+// (x) first                           (x) first
+// ( ) second                          (x) second
 
 
-todoList.toggleAll();
-todoList.displayTodos();
-My Todos:   //no (x) = false
-( ) first
-( ) second
+// todoList.toggleAll();
+// todoList.displayTodos();
+// My Todos:  no (x) = false
+// ( ) first
+// ( ) second
