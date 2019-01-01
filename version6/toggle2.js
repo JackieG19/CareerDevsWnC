f049ccacc79693f3a60a4e0fc0b31cd64849d2a8
Jackie <JGtheophila19@gmail.com>
2019-01-01 13:50:34 -0500

    toggle make everything true

--- version6/toggle2.js
+++ version6/toggle2.js
@@ -12,11 +12,10 @@ var todoList {
         
     if (this.todos[i].completed === true) {
         console.log('(x)', this.todos[i].todoText);
-           
-        }   
-            else {
-                console.log('( )', this.todos[i].todoText);
-           }
+       }   
+       else {
+           console.log('( )', this.todos[i].todoText);
+            }
         }
     },
 }
@@ -28,77 +27,57 @@ addTodos: function(todoText) {
         });
         this.displayTodos();
     },
-    
-changeTodos: function(position, todoText) {
-        this.todos[position].todoText = todoText;
-        this.displayTodos();
-    },
-
-deleteTodos: function(position) {
-        this.splice(position, 1);
-        this.displayTodos();
-    },
-
-toggleCompleted: function(position) {
-        var todo = this.todos[position];
-        todos.completed = !todos.completed;
-        this.displayTodos();
-},
 
 toggleAll: function(position) {
 
     var totalTodos = this.todos.length;
     var completedTodos = 0;
-    
-    // Get numbers of completed todos.
     for (var i = 0, i = totalTodos, i++){
       if (this.todos[i].completed === true){
         completedTodos++;
-        }//for
-    }//if
+        }
+    }
     
-
-    // Case 1: If everything's true, make everything false
-    if (completedTodos === totalTodos) {
-    
-    //make everything false.
-    for (var i = 0, i = totalTodos, i++){
+    // if completedTodos is equal to 0 /OR totalTodos
+    if (completedTodos === totalTodos) { 
+    for (var i = 0, i < totalTodos, i++){
       this.todos[i].completed = false){
         completedTodos++;
-        }//this
+        }
         
      // Case 2: Otherwise, make everything true  
         else {
-            for (var i = 0, i = totalTodos, i++){
+            for (var i = 0, i < totalTodos, i++){
+            // for loop togo through all the todos
                 this.todos[i].completed = true;
-                //for
-        }//else
-      }//for 
+                // make all todos true
+        }
+      }
       
       this.displayTodos();
       
-    }//if
-};//function
+    }
+};
 
 
 // console - prints
-todoList.addTodo('first')
-My Todos:
-( ) first
+// todoList.addTodo('first')
+// My Todos:
+// ( ) first
 
 
-todoList.addTodo('second')
-My Todos:
-( ) first
-( ) second
+// todoList.addTodo('second')
+// My Todos:
+// ( ) first
+// ( ) second
 
-todoList.toggleAll(); //true
-My Todos:
-(x) first
-(x) second
+// todoList.toggleAll(); - true
+// My Todos:
+// (x) first
+// (x) second
 
-todoList.toggleAll() // again = becomes false
-My Todos:
-( ) first
-( ) second
+// todoList.toggleAll() - again = becomes false
+// My Todos:
+// ( ) first
+// ( ) second
 
