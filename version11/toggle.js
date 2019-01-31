94f965b26fb87a55828bc4380cf78732a60f3e82
Jackie <JGtheophila19@gmail.com>
2019-01-30 21:42:45 -0500

    complete todo

--- version11/toggle.js
+++ version11/toggle.js
@@ -1,59 +1,42 @@
 var todoList {
     todos: [],
- /*  displayTodos function() {
-     if (this.todos.length === 0){
-        console.log('your todos list is empty!')
-     } 
-        else {
-        console.log("My Todos:");
-        for(var i = 0; i < this.todos.length; i++){
-        
-    if (this.todos[i].completed === true) {
-        console.log('(x)', this.todos[i].todoText);
-           
-        }   
-            else {
-                console.log('( )', this.todos[i].todoText);
-           }
-        }
-    },
-*/}
+}
     
-    addTodos: function(todoText) {
-        this.todos.push({
-        todoText: todoText,
-        completed: fasle
-        });
-    },
+addTodos: function(todoText) {
+    this.todos.push({
+    todoText: todoText,
+    completed: fasle
+    });
+},
     
-    changeTodos: function(position, todoText) {
-        this.todos[position].todoText = todoText;
-    },
+changeTodos: function(position, todoText) {
+    this.todos[position].todoText = todoText;
+},
 
-    deleteTodos: function(position) {
-        this.splice(position, 1);
-        //this.displayTodos();
-    },
+deleteTodos: function(position) {
+    this.splice(position, 1);
+    //this.displayTodos();
+},
+    
+toggleCompleted: function(position) {
+    var todo = this.todos[position];
+    todos.completed = !todos.completed;
+},
 
-    toggleCompleted: function(position) {
-        var todo = this.todos[position];
-        todos.completed = !todos.completed;
-    },
-
-    toggleAll: function(position) {
+toggleAll: function(position) {
     for (var i = 0, i = totalTodos, i++){
-      if (this.todos[i].completed === true){
+        if (this.todos[i].completed === true){
         completedTodos++;
-        }
     }
+}
     
-    if (completedTodos === totalTodos) {
+if (completedTodos === totalTodos) {
     for (var i = 0, i = totalTodos, i++){
-      this.todos[i].completed = false){
+        this.todos[i].completed = false){
         completedTodos++;
-        }else {
-            for (var i = 0, i = totalTodos, i++){
-                this.todos[i].completed = true;
+    }else {
+        for (var i = 0, i = totalTodos, i++){
+            this.todos[i].completed = true;
         }
       } 
 };
